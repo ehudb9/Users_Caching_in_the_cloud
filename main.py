2ca789fd7369691fa296faeeff07b6c59c5dd7d0
ehudbarda <ehud.barda@milab.idc.ac.il>
2021-06-19 00:24:22 +0300

    temp

---
+++ main.py
@@ -0,0 +1,121 @@
+# Assignment2 clouds
+
+import time
+import pandas
+import flask
+from flask import Flask, session, redirect, url_for, escape, request
+
+def put(str_key: str, data, expiration_date: int):
+    """
+    Manage the distribute: which user goes where
+
+    :param str_key:
+    :param data:
+    :param expiration_date:
+    :return:
+    """
+    pass
+
+
+def get(str_key: str) -> None or data:
+    """
+    Get the user from the relevant EC2 instance
+
+    :param str_key:
+    :return:
+    """
+    pass
+
+
+class CircularQueue():
+
+    # constructor
+    def __init__(self, size):  # initializing the class
+        self.size = size
+
+        # initializing queue with none
+        self.queue = [None for i in range(size)]
+        self.front = self.rear = -1
+
+    def enqueue(self, data):
+
+        # condition if queue is full
+        if ((self.rear + 1) % self.size == self.front):
+            print(" Queue is Full\n")
+
+        # condition for empty queue
+        elif (self.front == -1):
+            self.front = 0
+            self.rear = 0
+            self.queue[self.rear] = data
+        else:
+
+            # next position of rear
+            self.rear = (self.rear + 1) % self.size
+            self.queue[self.rear] = data
+
+    def dequeue(self):
+        if (self.front == -1):  # condition for empty queue
+            print("Queue is Empty\n")
+
+        # condition for only one element
+        elif (self.front == self.rear):
+            temp = self.queue[self.front]
+            self.front = -1
+            self.rear = -1
+            return temp
+        else:
+            temp = self.queue[self.front]
+            self.front = (self.front + 1) % self.size
+            return temp
+
+    def display(self):
+
+        # condition for empty queue
+        if (self.front == -1):
+            print("Queue is Empty")
+
+        elif (self.rear >= self.front):
+            print("Elements in the circular queue are:",
+                  end=" ")
+            for i in range(self.front, self.rear + 1):
+                print(self.queue[i], end=" ")
+            print()
+
+        else:
+            print("Elements in Circular Queue are:",
+                  end=" ")
+            for i in range(self.front, self.size):
+                print(self.queue[i], end=" ")
+            for i in range(0, self.rear + 1):
+                print(self.queue[i], end=" ")
+            print()
+
+        if ((self.rear + 1) % self.size == self.front):
+            print("Queue is Full")
+
+
+# Driver Code
+ob = CircularQueue(5)
+ob.enqueue(14)
+ob.enqueue(22)
+ob.enqueue(13)
+ob.enqueue(-6)
+ob.display()
+print("Deleted value = ", ob.dequeue())
+print("Deleted value = ", ob.dequeue())
+ob.display()
+ob.enqueue(9)
+ob.enqueue(20)
+ob.enqueue(5)
+ob.display()
+
+
+# This code is contributed by AshwinGoel
+def print_hi(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
