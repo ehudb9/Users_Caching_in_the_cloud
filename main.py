919aa39451a79b6b079f2052970fa3391b97e52c
taldanai1995 <70094025+taldanai1995@users.noreply.github.com>
2021-06-28 19:55:35 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,25 +8,56 @@ from botocore import exceptions
 import sys
 import random
 import xlrd
+import json
+
+# USER_NAME = input("User name\n")
+USER_NAME = "cc"
+
+def get_n_instances():
+    result = None
+    temp = ""
+    digits = "1234567890"
+    while result is None:
+        result = input("input a strictly positive number of instances\n")
+        for char in result:
+            if char not in digits:
+                temp = ""
+                result = None
+                break
+            else:
+                temp += char
+        else:
+            if temp == "0":
+                result = None
+            else:
+                result = temp
+    return int(result)
 
 
-
-PREFIX = "cache-elb-"
-USER_NAME = input("User name\n")
 path = "C:\\Temp\\" + str(USER_NAME) + "_AccessKeys.xltx"
+# nInstances = get_n_instances()
+nInstances = 3
+
+# print(path)
 def start():
     book = xlrd.open_workbook(path)
     sheet = book.sheet_by_index(0)
-    b = sheet.cell(1, 0).value
-    a = sheet.cell(1, 1).value
-    return a,b
-AWS_ACCESS , AWS_SECRET = start()
+    a = sheet.cell(1, 0).value
+    b = sheet.cell(1, 1).value
+    return a, b
+
+
+AWS_ACCESS, AWS_SECRET = start()
+
+
+# print(AWS_SECRET)
+# print(AWS_ACCESS)
 
 def init_security_groups(vpc_id):
     try:
-        response = ec2.describe_security_groups(GroupNames=[PREFIX + "elb-access"])
+        response = ec2.describe_security_groups(GroupNames=["-elb-access"])
         elb_access = response["SecurityGroups"][0]
-        response = ec2.describe_security_groups(GroupNames=[PREFIX + "instance-access"])
+        response = ec2.describe_security_groups(GroupNames=["cache-elb-instance-access"])
         instance_access = response["SecurityGroups"][0]
         return {
             "elb-access": elb_access["GroupId"],
@@ -41,7 +72,7 @@ def init_security_groups(vpc_id):
 
     elb = ec2.create_security_group(
         Description="ELB External Access",
-        GroupName=PREFIX + "elb-access",
+        GroupName="-elb-access",
         VpcId=vpc_id
     )
     elb_sg = boto3.resource('ec2').SecurityGroup(elb["GroupId"])
@@ -54,14 +85,14 @@ def init_security_groups(vpc_id):
 
     instances = ec2.create_security_group(
         Description="ELB Access to instances",
-        GroupName=PREFIX + "instance-access",
+        GroupName="cache-elb-instance-access",
         VpcId=vpc_id
     )
     instance_sg = boto3.resource('ec2').SecurityGroup(instances["GroupId"])
     instance_sg.authorize_ingress(
         CidrIp=cidr_block,
-        FromPort=8080,
-        ToPort=8080,
+        FromPort=80,
+        ToPort=80,
         IpProtocol="TCP",
     )
     return {
@@ -83,13 +114,13 @@ def get_default_subnets():
 def ensure_elb_setup_created():
     response = None
     try:
-        response = elb.describe_load_balancers(Names=["ELB"])
+        response = elb.describe_load_balancers(Names=["Elb-Python"])
     except exceptions.ClientError as e:
         if e.response['Error']['Code'] != 'LoadBalancerNotFound':
             raise e
         subnets = get_default_subnets()
         response = elb.create_load_balancer(
-            Name=PREFIX,
+            Name="Elb-Python",
             Scheme='internet-facing',
             IpAddressType='ipv4',
             Subnets=subnets,
@@ -104,20 +135,29 @@ def ensure_elb_setup_created():
     target_group = None
     try:
         target_group = elb.describe_target_groups(
-            Names=[PREFIX + "-tg"],
+            Names=["elb-tg"],
         )
     except exceptions.ClientError as e:
         if e.response['Error']['Code'] != 'TargetGroupNotFound':
             raise e
+        # TODO else if group found get the instances and return.
         target_group = elb.create_target_group(
-            Name=PREFIX + "-tg",
+            Name="elb-tg",
             Protocol="HTTP",
             Port=80,
             VpcId=vpc_id,
             HealthCheckProtocol="HTTP",
-            HealthCheckPort="8080",
-            HealthCheckPath="/health-check",
+            HealthCheckPort="80",
+            HealthCheckEnabled=True,
+            HealthCheckPath="/healthcheck",
+            HealthCheckIntervalSeconds=5,
+            HealthCheckTimeoutSeconds=2,
+            HealthyThresholdCount=2,
+            UnhealthyThresholdCount=2,
             TargetType="instance",
+            Matcher={
+                'HttpCode': '200',
+            }
         )
     target_group_arn = target_group["TargetGroups"][0]["TargetGroupArn"]
     listeners = elb.describe_listeners(LoadBalancerArn=elb_arn)
@@ -140,7 +180,7 @@ def ensure_elb_setup_created():
 def register_instance_in_elb(instance_id):
     results = ensure_elb_setup_created()
     target_group = elb.describe_target_groups(
-        Names=[PREFIX + "-tg"],
+        Names=["elb-tg"],
     )
     instance = boto3.resource('ec2').Instance(instance_id)
     sgs = [sg["GroupId"] for sg in instance.security_groups]
@@ -153,22 +193,46 @@ def register_instance_in_elb(instance_id):
         TargetGroupArn=target_group_arn,
         Targets=[{
             "Id": instance_id,
-            "Port": 8080
+            "Port": 80
         }]
     )
 
 
-def register_instances_in_elb(instances: list):
-    for instance in instances:
-        register_instance_in_elb(instance)
-
+def instances_manager():
+    res = ec2.describe_instances()
+    instances = []
+    for i in res["Reservations"]:
+        for instance in i["Instances"]:
+            # instances.append()
+            if instance["State"]["Name"] == "running":
+                instances.append(instance["InstanceId"])
+    if nInstances == len(instances):
+        for instance in instances:
+            register_instance_in_elb(instance)
+        return instances
+    # elif nInstances < len(instances):
+    if nInstances < len(instances):
+        instances1 = random.sample(instances, nInstances)
+        for instance in instances1:
+            register_instance_in_elb(instance)
+        return instances1
+    else:
+        # nInstances > instances
+        for instance in instances:
+            register_instance_in_elb(instance)
+        # create more and register
+        instancesToCreate = nInstances - len(instance)
+        # for i in range(instancesToCreate):
+            #TODO create and add instances while extracting the instance id + adding the base script
+        return instances # ++TODO list of new instances
 
 def get_targets_status():
     target_group = elb.describe_target_groups(
-        Names=[PREFIX + "-tg"],
+        Names=["elb-tg"],
     )
     target_group_arn = target_group["TargetGroups"][0]["TargetGroupArn"]
     health = elb.describe_target_health(TargetGroupArn=target_group_arn)
+    print(health)
     healthy = []
     sick = {}
     for target in health["TargetHealthDescriptions"]:
@@ -178,6 +242,9 @@ def get_targets_status():
             healthy.append(target["Target"]["Id"])
     return healthy, sick
 
+
 elb = boto3.client('elbv2', region_name='us-east-2', aws_access_key_id=AWS_ACCESS, aws_secret_access_key=AWS_SECRET)
 ec2 = boto3.client('ec2', region_name='us-east-2', aws_access_key_id=AWS_ACCESS, aws_secret_access_key=AWS_SECRET)
-ensure_elb_setup_created()
+instances_manager()
+#TODO need to find a way to connect the instances
+print(get_targets_status())
