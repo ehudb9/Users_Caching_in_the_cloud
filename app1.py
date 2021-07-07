42d9c4661a0c625142ddfb7c5c3861c82f635e52
taldanai1995 <70094025+taldanai1995@users.noreply.github.com>
2021-07-07 16:08:44 +0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -19,35 +19,6 @@ def health():
 def land():
     return "YES", 200
 
-
-@app.route('/get', methods=['GET'])
-def get():
-    #    url = f'http://{v.ip_address}:{v.port}/get?str_key={requests.args.get("str-key")}'
-    # get key from request
-    key = requests.args.get('str_key')
-    ans1 = requests.get("")
-
-
-
-@app.route('/put', methods=['POST'])
-def post():
-    url = f'http://{v.ip_address}:{v.port}/get?str_key={requests.args.get("str-key")}'
-
-
-@app.route('/get_all', methods=['GET'])
-def get():
-    url = f'http://{v.ip_address}:80/get_all'
-
-
-@app.route('/put_repart', methods=['POST'])
-def post():
-    url = f'http://{v.ip_address}:80/put_repart?str_key={requests.args.get("str-key")}&data={requests.args.get("data")}'
-
-@app.route('/put_repart', methods=['GET'])
-def post():
-    url = f'http://{v.ip_address}:80/put_repart?str_key={requests.args.get("str-key")}&data={requests.args.get("data")}'
-
-
 class Vars():
     def __init__(self):
         self.ip_address = requests.get('https://api.ipify.org').text
